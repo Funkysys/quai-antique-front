42c762e6d7f01cacf7ff6cb3794c73ec1e31ea9d
funkysys <aadelbos@gmail.com>
2023-02-20 19:58:33 +0100

    upgrade reservation component

--- Components/Reservation/Reservation.jsx
+++ Components/Reservation/Reservation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import styles from './Reservation.module.css';
@@ -9,19 +9,25 @@ const Reservation = () => {
     console.log(value);
     const [lunchOrDiner, setLunchOrDiner] = useState("");
     console.log(lunchOrDiner);
+    const handleOnClick = (event) => { setLunchOrDiner(event.target.innerText) }
 
     return (
         <div className={styles.container}>
-            <form action="" method="post" className={styles.form}>
+            <form className={styles.form}>
                 <Calendar onChange={onChange} value={value} />
                 <div className={styles.buttonContainer}>
                     <Button
+                        key="1"
                         variant="outline-primary"
-                        onClick={() => setLunchOrDiner("lunch")
-                        }>Lunch</Button>
-                    <Button variant="outline-primary"
-                        onClick={() => setLunchOrDiner("diner")
-                        }>Diner</Button>
+                        onClick={handleOnClick}
+                    >Lunch</Button>
+                    <Button
+                        key="2"
+                        variant="outline-primary"
+                        onClick={handleOnClick}
+                    >
+                        Diner
+                    </Button>
                 </div>
             </form>
         </div>
