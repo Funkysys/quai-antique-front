1772dba352153f7ef3140265b09bfb7a7dd53c92
funkysys <aadelbos@gmail.com>
2023-02-28 23:27:16 +0100

    upgrade openingHours component

--- Components/OpeningHours/OpeningHours.jsx
+++ Components/OpeningHours/OpeningHours.jsx
@@ -2,13 +2,12 @@
 import styles from './OpeningHours.module.css'
 
 const OpeningHours = ({ opening_hours }) => {
-  const opening = {};
+  const opening1 = {};
+  const opening2 = {}
   opening_hours['hydra:member'].map(elt => {
-
     let closeMinutes = elt.closeMinutes.minutes
     let openMinutes = elt.openMinutes.minutes
     let dayIndex;
-
     if (elt.day.day === "Lundi") {
       dayIndex = 1
     }
@@ -30,47 +29,113 @@ const OpeningHours = ({ opening_hours }) => {
     if (elt.day.day === "Dimanche") {
       dayIndex = 7
     }
-
     closeMinutes === 0 ? closeMinutes = "" : closeMinutes = elt.closeMinutes.minutes
     openMinutes === 0 ? openMinutes = "" : openMinutes = elt.openMinutes.minutes
-    if (elt.close) {
-      opening[elt.day.day] = ["on a aqua-poney", dayIndex]
-    } else {
+     if (!elt.close) {
       if (elt.lunch) {
-        opening[`${elt.day.day} midi`] = [`${elt.openingHours.hour} H ${openMinutes}`, `${elt.closeHours.hour} H ${openMinutes}`, dayIndex]
+        opening1[`${elt.day.day}`] = [`${elt.openingHours.hour} H ${openMinutes}`, `${elt.closeHours.hour} H ${openMinutes}`, dayIndex]
       } else if (elt.diner) {
-        opening[`${elt.day.day} soir`] = [`${elt.openingHours.hour} H ${openMinutes}`, `${elt.closeHours.hour} H ${closeMinutes}`, dayIndex]
+        opening2[`${elt.day.day}`] = [`${elt.openingHours.hour} H ${openMinutes}`, `${elt.closeHours.hour} H ${closeMinutes}`, dayIndex]
+      }
+    } else if (elt.close && elt.lunch) {
+       return opening1[elt.day.day] = ["on a aqua-poney", dayIndex]
+    } else if (elt.close && elt.diner) {
+      return opening2[elt.day.day] = ["Ce soir, on fait la bringue !", dayIndex]
+    } else if (elt.close) {
+      opening1[elt.day.day] = ["on a aqua-poney", dayIndex]
+      opening2[elt.day.day] = ["même le soir !", dayIndex]
+    } 
+  })
+
+  const openingArr1 = Object.entries(opening1)
+  const openingArr2 = Object.entries(opening2)
+  openingArr1.sort(function (a, b) {
+    if (a[1][2]) {
+      if (a[1][2] > b[1][2]) {
+        return 1
+      } else if (a[1][2] < b[1][2]) {
+        return -1
+      }
+    } else {
+      if (a[1][1] > b[1][2]) {
+        return 1
+      } else if (a[1][1] < b[1][2]) {
+        return -1
+      }
+    }
+  })
+  openingArr2.sort(function (a, b) {
+    if (a[1][2]) {
+      if (a[1][2] > b[1][2]) {
+        return 1
+      } else if (a[1][2] < b[1][2]) {
+        return -1
+      }
+    } else {
+      if (a[1][1] > b[1][2]) {
+        return 1
+      } else if (a[1][1] < b[1][2]) {
+        return -1
       }
     }
   })
 
-  const openingArr = Object.entries(opening)
-  openingArr.sort(function(a, b) {
-    if(a[1][2] > b[1][2]){
-      return 1
-    } else if (a[1][2] < b[1][2]) {
-      return -1
-    }
-  })
-
+  console.log(openingArr2)
 
   return (
     <div className={styles.openingContainer}>
       <h2 className={styles.openingTitle}>Horaires d'ouverture</h2>
-      {
-        openingArr.map(elt => {
-          return (
-            <>
-              {
-                elt[1][0] !== "on a aqua-poney" ?
-                  <p key={elt[1][2]}>{elt[0]} : {elt[1][0]} - {elt[1][1]}</p>
-                  :
-                  <p key={elt[1][1]}>{elt[0]} : {elt[1][0]}</p>
-              }
-            </>
-          )
-        })
-      }
+      <div className={styles.openingContainerLunchAndDiner}>
+        <div className={styles.openingLunch}>
+          {
+            openingArr1.map(elt => {
+              return (
+                <>
+                  {
+                    elt[1][0] !== "on a aqua-poney" ?
+                      <div className={styles.lunchContainer}>
+                        <div className={styles.lunchDay}>
+                          <p key={elt[1][2]}>{elt[0]} : </p>
+                        </div>
+                        <div className={styles.lunchHour}>
+                          <p> {elt[1][0]} - {elt[1][1]}</p>
+                        </div>
+                      </div>
+                      :
+                      <div className={styles.lunchContainer}>
+                        <div className={styles.lunchDay}>
+                          <p key={elt[1][1]}>{elt[0]} : </p>
+                        </div>
+                        <div className={styles.lunchHour}>
+                          <p>{elt[1][0]}</p>
+                        </div>
+                      </div>
+
+                  }
+                </>
+              )
+            })
+          }
+        </div>
+        <div >
+          {
+            openingArr2.map(elt => {
+              return (
+                <>
+                  {
+                    elt[1][0].length < 10 ?
+                      <div>
+                        <p>{elt[1][0]} - {elt[1][1]}</p>
+                      </div>
+                      :
+                      <p key={elt[1][1]}>{elt[1][0]}</p>
+                  }
+                </>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   )
 }
