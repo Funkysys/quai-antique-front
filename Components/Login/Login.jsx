2b233ff230cbfceb9c94cd6e9b9ef0758b199b2f
funkysys <aadelbos@gmail.com>
2023-03-08 20:31:32 +0100

    added sbimitReservationFunc

--- Components/Login/Login.jsx
+++ Components/Login/Login.jsx
@@ -3,14 +3,55 @@ import Button from 'react-bootstrap/Button';
 import styles from './Login.module.css'
 import { Context } from '@/lib/context';
 import Register from '../Register/Register'
-import loginfunc from '@/func/loginfunc';
 
 const Login = () => {
   const { dispatch } = useContext(Context);
   const [toggle, setToggle] = useState(false)
 
   const handleOnClick = () => setToggle(!toggle)
-  
+  async function loginfunc (e, dispatch, email = null, password = null) {
+    e.preventDefault()
+    let data = {}
+    if (email && password) {
+        data = {
+            email,
+            password
+        }
+    } else {
+        data = {
+            email: event.target.email.value,
+            password: event.target.password.value,
+        }
+    }
+    const JSONdata = JSON.stringify(data)
+
+    const endpoint = 'https://quai-antique.xyz/auth'
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Accept': '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSONdata,
+    }
+    const response = await fetch(endpoint, options)
+    console.log(response);
+    if (response.status == 200) {
+        const result = await response.json()
+        localStorage.setItem("token", `${result?.token}`)
+
+        const decode = jwtDecode(localStorage.token)
+        if (decode) {
+            await dispatch({
+                type: "LOGGED_IN_USER",
+                payload: decode
+            })
+        }
+    } else {
+        throw Error(response.statusText)
+    }
+}
   return (
     !toggle ?
         <div
