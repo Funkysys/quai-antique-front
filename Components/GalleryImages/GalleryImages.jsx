d3d7cb003769a6f3be3fd243adbe3b352203cfdc
funkysys <aadelbos@gmail.com>
2023-03-15 19:33:56 +0100

    added gallery component

--- Components/GalleryImages/GalleryImages.jsx
+++ Components/GalleryImages/GalleryImages.jsx
@@ -5,26 +5,24 @@ import styles from './GalleryImages.module.css'
 const GalleryImages = ({ images }) => {
   const [page, setPage] = useState(1)
   const [img, setImg] = useState([])
-  useEffect(() => {
-    images['hydra:member'].map(elt => {
-      setImg([])
-      const tempImg = { title: elt.title, url: `https://quai-antique.xyz/asset/images/gallery/${elt.imageName}`, alt: elt.imageAlt }
-      if(!img.includes(tempImg)) {
-        setImg(img => [...img, tempImg])
-      }
-    })
-  }, [])
+
   console.log(img);
-  // useEffect(() => {
-  //   if (page === 1) {
-  //     setImg([...img, { title: elt.title, url: `https://quai-antique.xyz/asset/images/gallery/${elt.imageName}`, alt: elt.imageAlt }])
-  //   } else {
-  //     const getImagesByPagination = async () => {
-  //       const res2 = await fetch('https://quai-antique.xyz/api/images')
-  //       const images = await res2.json()
-  //     }
-  //   }
-  // }, [page])
+  useEffect(() => {
+    if (page === 1) {
+      setImg([])
+      images['hydra:member'].map(elt => {
+        const tempImg = { title: elt.title, url: `https://quai-antique.xyz/asset/images/gallery/${elt.imageName}`, alt: elt.imageAlt }
+        if (!img.includes(tempImg)) {
+          setImg(img => [...img, tempImg])
+        }
+      })
+    } else {
+      const getImagesByPagination = async () => {
+        const res2 = await fetch('https://quai-antique.xyz/api/images')
+        const images = await res2.json()
+      }
+    }
+  }, [page])
   return (
     <div className={styles.container}>
       <div class="row">
